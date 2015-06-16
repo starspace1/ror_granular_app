9d3c7a095af354885d33617360d8b803378dca1e
Leslie Brown <lkb@alum.mit.edu>
2015-06-15 22:34:34 -0400

    Can now create, edit, destroy restaurants.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,8 +8,26 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant.update(params.require(:restaurant).permit(:name, :website))
+    redirect_to restaurants_path
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(params.require(:restaurant).permit(:name, :website))
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 end
