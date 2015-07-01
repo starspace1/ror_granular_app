9796eebbd946df338f09eef04fb0bd48f60360d8
Leslie Brown <lkb@alum.mit.edu>
2015-07-01 11:37:01 -0400

    Can only submit new restaurant form if no fields are empty

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  $('#new_restaurant').submit(function(){
+
+    // Check all the fields to make sure they're not empty
+    any_field_empty = $("#restaurant_name").val() == "" ||
+                      $("#restaurant_website").val() == "" ||
+                      $("#restaurant_low_price").val() == "" ||
+                      $("#restaurant_high_price").val() == "";
+
+    //Only submit the form if no fields are empty.
+    if(any_field_empty)
+    {
+      alert("You need to fill in all fields.");
+      event.preventDefault();
+    }
+    
+  });
+
+});
