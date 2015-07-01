8009fddee083e8d4d285fb731d875c5ececc630b
Leslie Brown <lkb@alum.mit.edu>
2015-07-01 11:46:01 -0400

    Added flash when new restaurant created.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -31,6 +31,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
 
     if @restaurant.save
+      flash[:notice] = "New restaurant created."
       redirect_to root_url
     else
       render 'new'
